d5dc3b4dcea3f1fdb6c25e89f75d20b6a69e193d
Prateek Deshmukh <prateek.deshmukh19@gmail.com>
2025-05-11 16:09:06 +0530

    model updated

--- script.py
+++ script.py
@@ -13,21 +13,23 @@ client = genai.Client(api_key=os.getenv("token"))
 
 destination = "/Users/prateek/notes"
 
+default = "gemini-2.5-flash-preview-04-17"
+pro = "gemini-2.5-pro-exp-03-25"
 
-
-def read(file):
+def read(file, model):
     reader = PdfReader(file)
     content = ""
-
+    print("Reading pdf...")
     for page in reader.pages:
         content+=page.extract_text()
 
-    generate_notes(content)
+    generate_notes(content, model)
 
 
-def generate_notes(content):
+def generate_notes(content, model):
+    print(f"Generating using {model}..")
     output = client.models.generate_content(
-    model="gemini-2.0-flash", contents=f"This is the content from a source PDF, generate descriptive and explanatory notes in beautiful markdown. Every page in the PDF is separated by \"--\". Content: {content}"
+    model=model, contents=f"This is the content from a source PDF, generate descriptive and explanatory notes in beautiful markdown. If you encounter a page with only title but no other content, assume there is an image in that page and elaborate from your knowledge base on that title. Content: {content}"
     )
     response = output.text
     if response.startswith("```markdown\n"):
@@ -45,8 +47,16 @@ def save(content):
     final(notes)
 
 def final(file):
+    print("Completed. Opening..")
     subprocess.run(['open', file], check=True)
 
 if __name__ == "__main__":
     pdf_file_path = sys.argv[1]
-    read(pdf_file_path)
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "-p":
+            model = pro
+        else:
+            model = default
+    else:
+        model = default
+    read(pdf_file_path, model)
